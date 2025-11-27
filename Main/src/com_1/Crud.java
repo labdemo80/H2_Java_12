de80633e916c0a3ddbd516afc0a86b98f2d9f47e
labdemo80 <labdemo80@gmail.com>
2025-11-27 08:54:31 +0530

    new commit

--- Main/src/com_1/Crud.java
+++ Main/src/com_1/Crud.java
@@ -4,7 +4,28 @@ interface Operations{
 	void add();
 	void sub();
 	void div();
-	void mul();
+}
+
+class Test implements Operations{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void div() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
 
 public class Crud {
